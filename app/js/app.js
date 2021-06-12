18564836a630873674ac94ef5b9ec0b2c2e53fd4
Mawoka <mawoka@e.email>
2021-06-12 11:12:55 +0200

    Updated

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,10 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
+var do_req = true
+var active_btn = null
+var buttonsAvailable = ["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10", "b11", "b12"]
+
 function onDeviceReady() {
-    console.log(navigator.vibrate);
     // Cordova is now initialized. Have fun!
     if (localStorage.getItem("analytics") == "all") {
         var Sentry = cordova.require("sentry-cordova.Sentry");
@@ -14,15 +17,46 @@ function onDeviceReady() {
         var Sentry = cordova.require("sentry-cordova.Sentry");
         Sentry.init({ dsn: 'https://790b7fe9e12c41f0b9778bd467b30463@o661934.ingest.sentry.io/5802311' });
     }
+    console.log("Let's go!")
+
+    for (let i=1; i <= 12; i++ ) {
+        
+        if (localStorage.getItem(`btn-text-${i}`) != null) {
+            console.log(i)
+            document.getElementById(`b${i}`).innerHTML = localStorage.getItem(`btn-text-${i}`)
+        }
+    }
+    for (let i=1; i <= 12; i++ ) {
+        if (localStorage.getItem(`btn-color-${i}`) != null) {
+            console.log(i)
+            console.log(`btn-color-${i}`)
+            document.getElementById(`b${i}`).style.backgroundColor = localStorage.getItem(`btn-color-${i}`)
+        }
+    }
+
+
 }
 
-
-document.addEventListener("keypress", (e) => {
-    buttonPressed((e.code).replace("Key", "").toLowerCase())
-    navigator.vibrate(300)
+document.addEventListener("long-press", function(e) {
+    window.location.replace("settings.html")
 })
 
+
+
+
+
+
+
+
+
+
+function save_settings() {
+    console.log("Here i am")
+    localStorage.setItem(`btn-text-${active_btn}`, document.getElementById("btn_label").value)
+}
+
 function buttonPressed(btn) {
+    navigator.vibrate(300)
     var xhr = new XMLHttpRequest()
     xhr.open("GET", `${localStorage.getItem("host")}/button_pressed?key=${btn}&token=${localStorage.getItem("pin")}`)
     xhr.send()
@@ -30,3 +64,43 @@ function buttonPressed(btn) {
         M.toast({text: "There was an error sending the request!"})
     }
 }
+
+
+
+
+
+document.addEventListener("long-press", function(e){ 
+    console.log("L67")
+    
+    do_req = false
+    var elems = document.querySelectorAll('.modal');
+    var elem = document.getElementById("settings")
+    var instances = M.Modal.init(elems, {dismissable: false, startingTop: "10%", endingTop: "10%"})
+    var instance = M.Modal.getInstance(elem);
+    instance.open()
+    active_btn = e.target.id
+    console.log(active_btn)
+    console.log("L76")
+    
+    if (e.target.id == "b1") {
+        console.log("L79")
+        var hueb = new Huebee( ".color-input", {
+            setText: false,
+            staticOpen: true,
+            notation: 'hex',
+            saturations: 2,
+        } )
+        
+    }
+    do_req = true
+})
+
+
+
+document.addEventListener("keypress", (e) => {
+    if (do_req == true) {
+        buttonPressed((e.code).replace("Key", "").toLowerCase())
+        
+    }
+
+})
---
+++ app/js/settings.js
@@ -0,0 +1,59 @@
+var hueb = undefined
+
+
+var elems = document.querySelectorAll('.color-input');
+var hueb = new Array()
+for (var i = 0; i < elems.length; i++) {
+
+  var elem = elems[i];
+  console.log(elems[i], i)
+  hueb.push(new Huebee(elem, {
+    setText: false,
+    staticOpen: false,
+    notation: 'hex',
+    saturations: 1,
+  }));
+}
+
+
+function saveSettings(ButtonNumber) {
+
+  if (hueb[ButtonNumber - 1].color != undefined) {
+    localStorage.setItem(`btn-color-${ButtonNumber}`, hueb[ButtonNumber - 1].color)
+  }
+
+  if (document.getElementById(`buttonName${ButtonNumber}`).value != "") {
+    localStorage.setItem(`btn-text-${ButtonNumber}`, document.getElementById(`buttonName${ButtonNumber}`).value)
+  }
+  window.location.reload()
+}
+
+
+
+document.addEventListener('deviceready', onDeviceReady, false);
+function onDeviceReady() {
+  for (let i = 1; i <= 12; i++) {
+    if (localStorage.getItem(`btn-text-${i}`) != null) {
+      document.getElementById(`b${i}`).innerHTML = localStorage.getItem(`btn-text-${i}`)
+    }
+  }
+  for (let i = 1; i <= 12; i++) {
+    if (localStorage.getItem(`btn-color-${i}`) != null) {
+      console.log(`btn-color-${i}`)
+      document.getElementById(`b${i}`).style.backgroundColor = localStorage.getItem(`btn-color-${i}`)
+    }
+  }
+
+}
+
+document.getElementById("ResetButton").addEventListener("long-press", function (e) {
+  localStorage.clear()
+  window.location.replace("index.html")
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems, {
+    // specify options here
+  });
+});
