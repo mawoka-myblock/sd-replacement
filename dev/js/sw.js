4287087f3b2e5de2ab7c7f8a61f2c835726407c2
mawoka-myblock <mawoka-myblock@users.noreply.github.com>
2021-06-26 09:20:37 +0000

    deploy: 51da3d4498c6cf7845a141cdd3b2343072c3f6cb

---
+++ dev/js/sw.js
@@ -0,0 +1,7 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
+
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === "image",
+    new workbox.strategies.CacheFirst()
+)
